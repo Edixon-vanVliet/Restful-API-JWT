15c531d44d90d21cd40b3a476a76df15eedfbcc8
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-07-18 11:23:49 -0400

    Reorganize validators folder

--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 
 const { addUser } = require("../controllers/account");
 const validators = require("../validators");
-const { validateCreateUserFields } = require("../validators/users");
+const { validateCreateUserFields } = require("../validators/users/createUsers");
 
 Router.post("/register", validators(validateCreateUserFields), addUser);
 
